3093c5b9a3af9dbecbf87e1eaf18f91b2586eee2
Bassam Ismail <skippednote@gmail.com>
2021-07-27 15:21:28 +0530

    build(lint): run tsc with noEmit when linting

--- generators/app/templates/copy/gulp-tasks/lint.js
+++ generators/app/templates/copy/gulp-tasks/lint.js
@@ -1,21 +1,28 @@
 const stylelint = require('gulp-stylelint');
 const eslint = require('gulp-eslint');
 const gulpIf = require('gulp-if');
+const typescript = require('gulp-typescript');
+const ts = typescript.createProject('tsconfig.json', { noEmit: true });
 
 module.exports = (gulp, config) => {
-  gulp.task('lint:scss', () =>
+  gulp.task('lint:stylelint', () =>
     gulp.src(config.scss.all).pipe(stylelint(config.stylelint.options)),
   );
 
-  gulp.task('lint:ts', () =>
+  gulp.task('lint:eslint', () =>
     gulp
       .src(config.ts.source)
       .pipe(eslint())
       .pipe(gulpIf(process.env.CI === 'true', eslint.failOnError())),
   );
 
+  gulp.task('lint:tsc', () => gulp.src(config.ts.source).pipe(ts()));
+
   gulp.task(
     'lint',
-    gulp.series('prettier', gulp.parallel('lint:scss', 'lint:ts')),
+    gulp.series(
+      'prettier',
+      gulp.parallel('lint:stylelint', 'lint:eslint', 'lint:tsc'),
+    ),
   );
 };
